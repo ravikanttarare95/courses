358eb55098d070bdf7e12179b1dda7639c20939a
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-27 15:52:48 +0530

    Added Input validation

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ let courseDate = document.getElementById("course-date");
 let courseTime = document.getElementById("course-time");
 let courseInstructor = document.getElementById("course-instructor-name");
 let coursePrice = document.getElementById("course-price");
+
 let existingCards = JSON.parse(localStorage.getItem("CourseCards")) || [];
+
 function addcard() {
+  if (
+    !courseTitle.value ||
+    !courseDescription.value ||
+    !courseDate.value ||
+    !courseTime.value ||
+    !courseInstructor.value ||
+    !coursePrice.value
+  ) {
+    alert("Please fill in all fields.");
+    return;
+  }
+
   let newCard = {
     courseTitle: courseTitle.value,
     courseDescription: courseDescription.value,
