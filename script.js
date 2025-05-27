3170572d91a48ab0a730f3728c9012fb04140df7
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-27 11:55:46 +0530

    Added Form

---
+++ script.js
@@ -0,0 +1,11 @@
+const cards = [
+  {
+    title: "Card 1",
+    description: "This is the first card.",
+    date: "2023-10-01",
+    time: "10:00 AM",
+    Instrucor: "John Doe",
+    price: "$10",
+    button: "know More",
+  },
+];
